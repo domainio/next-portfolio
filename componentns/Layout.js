faa3af9870b834dc12351bbdc13fa1cdba3ef410
domainio <omega@omegas-MacBook-Pro.local>
2020-04-23 15:58:30 +0300

    css style jsx

--- componentns/Layout.js
+++ componentns/Layout.js
@@ -1,15 +1,49 @@
 import Link from 'next/link';
 
 const Layout = ({ children, title }) => (
-  <div>
+  <div className="root">
     <header>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/hireme">Hire Me</Link>
     </header>
     <h1>{title}</h1>
-    {children}
+    <div>{children}</div>
     <footer>&copy; {new Date().getFullYear()}</footer>
+    <style jsx>{`
+      .root {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      }
+      header {
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        padding: 1em;
+        font-size: 1.2rem;
+        background: indigo;
+      }
+      header a {
+        color: darkgray;
+        text-decoration: none;
+      }
+      header a:hober {
+        font-weight: bold;
+        color: lightgray;    
+      }
+      footer {
+        padding: 1em;
+      }
+    `}</style>
+    <style global jsx>{`
+      body {
+        margin: 0;
+        font-size: 110%;
+        background: #f0f0f0;
+      }
+    `}</style>
   </div>
 )
 
