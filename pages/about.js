87e2b102684b8f3abe1b711bb2093fec5dd69821
domainio <omega@omegas-MacBook-Pro.local>
2020-04-23 14:57:58 +0300

    add next.js link tag to nav between pages

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
 const About = () => (
   <div>
     <h1>About</h1>
-    <p>A javascript prog</p>
-    <img src="/static/smart-guy.jpeg" alt="Smart Guy"  />
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+    <p>A javascript proj</p>
+    <img src="/static/smart-guy.jpeg" alt="Smart Guy" />
   </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 const Index = () => (
-  <div>hi world x</div>
+  <div>
+    <h1>Home</h1>
+    <Link href="/about">
+      <a>About</a>
+    </Link>
+    <p>hi world</p>
+  </div>
 );
 
 export default Index;
