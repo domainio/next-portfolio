1d7f97eb0cbd0bd343abfa461fc41a72801a1fb3
domainio <omega@omegas-MacBook-Pro.local>
2020-04-28 14:11:27 +0300

    Fetching data with getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,23 @@
 import Link from 'next/link';
 import Layout from '../componentns/Layout';
+import fetch from 'isomorphic-unfetch';
 
-const About = () => (
-  <Layout title="About">
-    <p>A javascript proj</p>
-    <img src="/static/smart-guy.jpeg" alt="Smart Guy" />
-  </Layout>
-);
+const About = ({ user }) => {
+  console.log(user);
+  return (
+    <Layout title="About">
+      <p>{user.name}</p>
+      <img src={user.avatar_url} alt="Erez Zohar" />
+    </Layout>
+  );
+}
+
+About.getInitialProps = async () => {
+  const res = await fetch('https://api.github.com/users/domainio');
+  const user = await res.json();
+  return {
+    user
+  }
+}
 
 export default About;
